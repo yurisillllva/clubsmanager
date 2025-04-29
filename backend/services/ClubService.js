8aa2b8d24327ce9e722d726591f255d34e2e3d95
Yuri Silva <ysm281@gmail.com>
2025-04-29 12:22:12 -0300

    file created to contain business logic

---
+++ backend/services/ClubService.js
@@ -0,0 +1,38 @@
+const ClubRepository = require('../repositories/ClubRepository');
+const ClubResource = require('../resources/ClubResource');
+
+module.exports = {
+  async getAllClubs() {
+    const clubs = await ClubRepository.findAll();
+    return clubs.map(club => ClubResource.toJson(club));
+  },
+
+  async getClubById(id) {
+    const club = await ClubRepository.findById(id);
+    return club ? ClubResource.toJson(club) : null;
+  },
+
+  async createClub(clubData) {
+    const existingClub = await ClubRepository.findByEmail(clubData.email);
+    if (existingClub) {
+      throw new Error('Email já está em uso');
+    }
+    const newClub = await ClubRepository.create(clubData);
+    return ClubResource.toJson(newClub);
+  },
+
+  async updateClub(id, clubData) {
+    if (clubData.email) {
+      const existingClub = await ClubRepository.findByEmail(clubData.email);
+      if (existingClub && existingClub.id !== id) {
+        throw new Error('Email já está em uso');
+      }
+    }
+    await ClubRepository.update(id, clubData);
+    return this.getClubById(id);
+  },
+
+  async deleteClub(id) {
+    return ClubRepository.delete(id);
+  }
+};
