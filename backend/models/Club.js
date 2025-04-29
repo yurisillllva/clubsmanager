b1ae6b33f3d1dd15e15ff1cc19ae13597aedaf71
Yuri Silva <ysm281@gmail.com>
2025-04-29 13:39:19 -0300

    fix repository

--- backend/models/Club.js
+++ backend/models/Club.js
@@ -1,4 +1,6 @@
-module.exports = (sequelize, DataTypes) => {
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
     const Club = sequelize.define('Club', {
       nome: {
         type: DataTypes.STRING,
