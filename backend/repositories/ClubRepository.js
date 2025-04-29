2c6a213b0304bb18ccb4be597f7e7cba41a41821
Yuri Silva <ysm281@gmail.com>
2025-04-29 14:04:31 -0300

    fix to access registered model and pagination implementation

--- backend/repositories/ClubRepository.js
+++ backend/repositories/ClubRepository.js
@@ -1,7 +1,15 @@
-const Club = require('../models/Club');
+const { Sequelize } = require('sequelize');
+const sequelize = require('../config/database');
+const Club = sequelize.models.Club;
 
 module.exports = {
-  findAll: () => Club.findAll(),
+  findAll: (page = 1, search = '') => Club.findAndCountAll({
+    where: search ? { nome: { [Sequelize.Op.iLike]: `%${search}%` } } : {},
+    limit: 7,
+    offset: (page - 1) * 7,
+    order: [['nome', 'ASC']]
+  }),
+
   findById: (id) => Club.findByPk(id),
   findByEmail: (email) => Club.findOne({ where: { email } }),
   create: (clubData) => Club.create(clubData),
