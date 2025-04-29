ebeeb391ef9dca80c4a56e8d39771644cda25c39
Yuri Silva <ysm281@gmail.com>
2025-04-29 09:53:53 -0300

    created model club

---
+++ models/Club.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+    const Club = sequelize.define('Club', {
+      nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      telefone: DataTypes.STRING,
+      cidade_sede: DataTypes.STRING,
+      data_criacao: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowUpdate: false
+      }
+    }, {
+      timestamps: false,
+      tableName: 'clubes'
+    });
+  
+    return Club;
+  };
