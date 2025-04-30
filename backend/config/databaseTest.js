a4eab600a50ee44028f88382d43b5db4891f0296
Yuri Silva <ysm281@gmail.com>
2025-04-30 02:08:01 -0300

    file database test

---
+++ backend/config/databaseTest.js
@@ -0,0 +1,19 @@
+require('dotenv').config({ path: '.env.test' });
+
+module.exports = {
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+};
